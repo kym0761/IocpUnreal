df6884c0ca11ffd5d280e53bbe78067693a56c21
kym <kym0761@naver.com>
2024-03-03 18:07:37 +0900

    Add Unreal

---
+++ IocpTest/Source/IocpTest/TestGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TestGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IOCPTEST_API UTestGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
